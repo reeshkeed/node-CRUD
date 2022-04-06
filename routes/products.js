8cbec32ab00f8fd32f482073bbfcce93bd644f18
reeshkeed <hello@girishdaloso.com>
2022-04-06 10:22:45 +0800

    Created update product

--- routes/products.js
+++ routes/products.js
@@ -26,4 +26,24 @@ router.post('/', async (req, res) => {
   res.send(product);
 });
 
+// PUT: update a product
+router.put('/:id', async (req, res) => {
+  // Validate request body
+  // If validation failed return 200
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Else find id and update
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, price: req.body.price },
+    { new: true }
+  );
+
+  // Return 404 if product id not found
+  if (!product) return res.status(404).send('Product not found.');
+
+  res.send(product);
+});
+
 module.exports = router;
